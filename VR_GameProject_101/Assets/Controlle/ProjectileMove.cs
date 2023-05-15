56b6a76fbd9282985ed4913109201e1cd97b857b
23jihwan <dbwlghks7410@naver.com>
2023-05-15 12:20:20 +0900

    펀칭

--- VR_GameProject_101/Assets/Controlle/ProjectileMove.cs
+++ VR_GameProject_101/Assets/Controlle/ProjectileMove.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class ProjectileMove : MonoBehaviour
 {
+
+
+    public enum PROJECTILETYPE                                      //총알 타입 구분을 위해 Enum 선언
+    {
+        PLAYER,
+        ENEMY
+    }
+
+
     public Vector3 launchDirection;
+    public PROJECTILETYPE projectileType = PROJECTILETYPE.PLAYER;   //총알 타입 선언
+    
 
 
-    private void OnCollisionEnter(Collision collision)
+
+    //private void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider other)           //Trigger 함수
     {   //벽에 충돌시 파괴
-        if(collision.gameObject.name=="Wall")
+        if(other.gameObject.name=="Wall")         
         {
             Destroy(this.gameObject);
         }
         //몬스터에 충돌시
-        if (collision.gameObject.name == "Monster")
+        if (other.gameObject.name == "Monster" && projectileType == PROJECTILETYPE.PLAYER)
         {
             //몬스터에게 데미지를 주고 사라진다.
-            collision.gameObject.GetComponent<MonsterController>().Damanged(1);
+            other.gameObject.GetComponent<MonsterController>().Damanged(1);
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.tag == "Player" && projectileType == PROJECTILETYPE.ENEMY)
+        {
+            //플레이어에게 데미지를 주고 사라진다
+            other.gameObject.GetComponent<PlayerControlle>().Damanged(1);
+            Destroy(this.gameObject);
         }
     }
     private void FixedUpdate()
